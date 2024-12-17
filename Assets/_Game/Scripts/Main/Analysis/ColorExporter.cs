3680b9c926829973fda1305698782909317ba0cd
Gamba <juanca.quinterom@gmail.com>
2024-12-16 22:11:31 -0500

    Improved CSV formatting

--- Assets/_Game/Scripts/Main/Analysis/ColorExporter.cs
+++ Assets/_Game/Scripts/Main/Analysis/ColorExporter.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -9,12 +8,39 @@ public static class ColorExporter
     {
         using (StreamWriter stream = new StreamWriter(path))
         {
-            foreach (Color color in colors)
+            List<string> data = GetExportData(colors);
+
+            foreach (string row in data)
             {
-                stream.WriteLine($"{color.r};{color.g};{color.b};{color.a}");
+                stream.WriteLine(row);
             }
 
             stream.Close();
         }
+    }
+
+    private static List<string> GetExportData(List<Color> colors)
+    {
+        List<string> data = new List<string>();
+
+        data.Add("R;G;B");
+
+        foreach (Color color in colors)
+        {
+            string r = FormatColorComponent(color.r);
+            string g = FormatColorComponent(color.g);
+            string b = FormatColorComponent(color.b);
+
+            data.Add($"{r};{g};{b}");
+        }
+
+        return data;
+    }
+
+    private static string FormatColorComponent(float value)
+    {
+        value = Mathf.Round(value * 100);
+
+        return $"{value}%";
     }
 }
